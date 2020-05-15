280b65daabe89ad4acccf74fb288bc8740e517f4
olusola-adio <olusola_adio@hotmail.com>
2020-05-15 18:36:25 +0000

    test

--- profile.ps1
+++ profile.ps1
@@ -17,4 +17,4 @@
 
 # Uncomment the next line to enable legacy AzureRm alias in Azure PowerShell.
 # Enable-AzureRmAlias
-# You can also define functions or aliases that can be referenced in any of your PowerShell functions.
+# You can also define functions or aliases that can be referenced in any of your PowerShell functions
